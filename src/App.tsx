e6b1c5d5a08e5cb90bfdc768081a289ed8e6adf7
Strykeros <edijsz@tutanota.com>
2025-09-13 20:39:37 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import Header from './components/Header'
+import './App.css'
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
+const App = () => {
+
+  return (
+    <>
+      <Header></Header>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header.tsx
@@ -0,0 +1,72 @@
+import {
+  AppBar,
+  IconButton,
+  Toolbar,
+  Typography,
+  Box,
+  Button,
+  Menu,
+  MenuList,
+  MenuItem,
+} from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
+import { useState, type MouseEvent } from "react";
+
+const menuItemTexts = ["Home", "Blog", "About us", "Contact"];
+
+const Header = () => {
+  const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+
+  const handleOpenNavMenu = (e: MouseEvent<HTMLElement>) => {
+    setAnchorElNav(e.currentTarget);
+  };
+  const handleCloseNavMenu = () => setAnchorElNav(null);
+
+  return (
+    <AppBar position="fixed" sx={{ bgcolor: "green", color: "white" }}>
+      <Toolbar  sx={{display: {xs: 'flex', justifyContent: 'space-between'}}}>
+        <Typography variant="h4">
+          Codnity test
+        </Typography>
+        <IconButton
+          size="large"
+          edge="end"
+          aria-label="menu"
+          sx={{ color: "white", display: { xs: "flex", md: "none" } }}
+          onClick={handleOpenNavMenu}
+        >
+          <MenuIcon />
+        </IconButton>
+        <Menu
+          open={Boolean(anchorElNav)}
+          onClose={handleCloseNavMenu}
+          sx={{ display: { xs: "flex", md: "none" } }}
+          anchorEl={anchorElNav}
+          anchorOrigin={{
+            vertical: "bottom",
+            horizontal: "left",
+          }}
+          keepMounted
+          transformOrigin={{
+            vertical: "top",
+            horizontal: "left",
+          }}
+        >
+          <MenuList>
+            {menuItemTexts.map((text) => (
+              <MenuItem sx={{ color: "inherit" }}>{text}</MenuItem>
+            ))}
+          </MenuList>
+        </Menu>
+
+        <Box sx={{ display: { xs: "none", md: "flex" } }}>
+          {menuItemTexts.map((text) => (
+            <Button sx={{ color: "inherit" }}>{text}</Button>
+          ))}
+        </Box>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Header;
